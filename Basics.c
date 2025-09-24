1fced2e634cffeee04c233d7c33bdcdcfdf744bf
unknown User 404 <Bhavneet11d@gmail.com>
2025-09-24 16:10:28 -0400

    Update Basics.c

--- Basics.c
+++ Basics.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-int main() {
- int age =22;
- printf( "Bhavneet is  %d years old ", age );
-return 0;
+
+void main()
+{
+    int i = 10;
+    printf("This is my integer: %d \n", i);
 }
+// The %d is to tell printf() function to format the integer i as a decimal number. The output from this program would be This is my integer: 10.
