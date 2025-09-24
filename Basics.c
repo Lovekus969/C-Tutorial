c4c8ade84610630c87ef5839716604da35e2fc8e
unknown User 404 <Bhavneet11d@gmail.com>
2025-09-24 15:30:56 -0400

    Create Basics.c

---
+++ Basics.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Mr Kush is here to write the next level c code");
+
+    return 0;
+}
