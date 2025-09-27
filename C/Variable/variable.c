e3deb90e902362120133c0cce90e3d67443343bb
unknown User 404 <Bhavneet11d@gmail.com>
2025-09-27 13:01:08 -0400

    Create variable.c

---
+++ C/Variable/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;        // integer variable
+    float pi = 3.14;     // floating-point variable
+    char grade = 'A';    // character variable
+
+    printf("Age: %d\n", age);
+    printf("Pi: %f\n", pi);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
